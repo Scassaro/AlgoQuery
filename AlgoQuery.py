d01062e5fb34ca2958140fd5bdc1266bdc8776ba
Stephen Cassaro <srcassaro@gmail.com>
2023-12-02 20:25:08 -0800

    Added mac support

--- AlgoQuery.py
+++ AlgoQuery.py
@@ -1,3 +1,13 @@
 from urllib.request import urlopen
-
+import requests
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+    
 url = "https://algoexplorer.io/"
+response = requests.get(url, headers={'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/533.44 (KHTML, like Gecko) Chrome/47.0.2360.241 Safari/601'})
+print()
+# myopener = MyOpener()
+# page = myopener.urlopen(url)
+# html_bytes = page.read()
+# html = html_bytes.decode("utf-8")
+# print(html)
